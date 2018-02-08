0a8d06de0bf2a4c6bf86641392198eab626a6232
panchaltej <tejaspanchal49@gmail.com>
2018-02-08 10:08:33 -0800

    Added file to establish connection with mongodb

--- app.js
+++ app.js
@@ -4,15 +4,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var multer = require('multer');
 var index = require('./routes/index');
-var login = require('./routes/login');
-var signup = require('./routes/signup');
-var user = require('./routes/user');
-var files = require('./routes/files');
-var folder = require('./routes/folder');
-var uploadFiles = require('./routes/uploadFiles');
-var activity = require('./routes/activity');
 
 
 var app = express();
@@ -33,13 +25,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
-app.use('/login', login);
-app.use('/signup', signup);
-app.use('/user', user);
-app.use('/files', files);
-app.use('/folder', folder);
-app.use('/uploadFiles', uploadFiles);
-app.use('/activity', activity);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
